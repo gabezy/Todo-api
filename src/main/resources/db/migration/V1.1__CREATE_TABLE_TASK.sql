e9411afac39bf7af4f8de2606a27bb2802ae2787
gabezy <gabrielmoreira2705@gmail.com>
2024-12-30 21:01:45 -0300

    add flyway

---
+++ src/main/resources/db/migration/V1.1__CREATE_TABLE_TASK.sql
@@ -0,0 +1,8 @@
+-- CREATE TABLE --
+
+CREATE TABLE TASK
+(
+    IDT_TASK  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT 'Task unique ID. Auto increment.',
+    CONTENT   VARCHAR(400) NOT NULL COMMENT 'Content of the task.',
+    COMPLETED BOOLEAN      NOT NULL COMMENT 'Mark as the taks is completed or not. 0 = not completed; 1 = completed.'
+) ENGINE=InnoDB;
