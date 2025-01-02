65b8f78cc505090818107608db5cb0c902f07709
gabezy <gabrielmoreira2705@gmail.com>
2025-01-01 21:25:57 -0300

    add validation

--- src/main/java/br/com/gabezy/todoapi/domain/dto/TaskDTO.java
+++ src/main/java/br/com/gabezy/todoapi/domain/dto/TaskDTO.java
@@ -1,7 +1,12 @@
 package br.com.gabezy.todoapi.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record TaskDTO(
+        @NotBlank
         String content,
+        @NotNull
         Boolean completed
 ) {
 }
