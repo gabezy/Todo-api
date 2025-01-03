b89945bffbfdd924c5e62962c09021313828b43a
gabezy <gabrielmoreira2705@gmail.com>
2025-01-03 01:25:45 -0300

    add handle to exception invalid fields

---
+++ src/main/java/br/com/gabezy/todoapi/config/expectionhandler/ControllerExceptionHandler.java
@@ -0,0 +1,44 @@
+package br.com.gabezy.todoapi.config.expectionhandler;
+
+import br.com.gabezy.todoapi.domain.enumaration.ErrorCode;
+import br.com.gabezy.todoapi.exceptions.ResourceNotFoundException;
+import org.springframework.http.*;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final ErrorCode INVALID_FIELDS = ErrorCode.INVALID_FIELDS;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> fields = ex.getAllErrors().stream()
+                .filter(FieldError.class::isInstance)
+                .map(FieldError.class::cast)
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+        var error = new ResponseError(INVALID_FIELDS.name(), INVALID_FIELDS.getMessage(), fields);
+        return this.handleExceptionInternal(ex, error, new HttpHeaders(headers), status, request);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        var error = new ResponseError(ErrorCode.TASK_NOT_FOUND.name(), ex.getMessage(), Collections.emptyMap());
+        return this.handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
+    }
+
+}
