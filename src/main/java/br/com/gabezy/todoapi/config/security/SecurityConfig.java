5d086a8209e722596a43fd5217404947701579ed
gabezy <gabrielmoreira2705@gmail.com>
2025-01-08 23:46:30 -0300

    add /auth public endpoint

--- src/main/java/br/com/gabezy/todoapi/config/security/SecurityConfig.java
+++ src/main/java/br/com/gabezy/todoapi/config/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.gabezy.todoapi.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,6 +39,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(request ->
                     request.requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                            .requestMatchers(HttpMethod.POST, "/auth").permitAll()
                             .anyRequest().authenticated()
                 )
                 .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
