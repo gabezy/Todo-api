4d9a417e1065cf0ff9e32efd287d62a7c50b223d
gabezy <gabrielmoreira2705@gmail.com>
2025-01-28 23:07:13 -0300

    add endpoint utility class

---
+++ src/main/java/br/com/gabezy/todoapi/utils/EndpointUtil.java
@@ -0,0 +1,14 @@
+package br.com.gabezy.todoapi.utils;
+
+public class EndpointUtil {
+
+    public static final String[] PUBLIC_ENDPOINTS = {
+            "/api-docs/**", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/swagger-ui/index.html"
+    };
+
+    public static final String[] PUBLIC_POST_ENDPOINTS = {
+            "/auth", "/users",
+    };
+
+    private EndpointUtil() { throw new IllegalStateException("Utility class");}
+}
