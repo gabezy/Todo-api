d929ff1a60bc4c946a697c6b483a306f7290b3aa
gabezy <gabrielmoreira2705@gmail.com>
2025-01-29 22:38:45 -0300

    bypass swagger endpoint authentication in the AuthenticationFilter

--- src/main/java/br/com/gabezy/todoapi/utils/EndpointUtil.java
+++ src/main/java/br/com/gabezy/todoapi/utils/EndpointUtil.java
@@ -3,7 +3,8 @@ package br.com.gabezy.todoapi.utils;
 public class EndpointUtil {
 
     public static final String[] PUBLIC_ENDPOINTS = {
-            "/api-docs/**", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/swagger-ui/index.html"
+            "/api-docs/**", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/swagger-ui/index.html",
+            "/swagger-ui/", "/api-docs", "/v3/api-docs/"
     };
 
     public static final String[] PUBLIC_POST_ENDPOINTS = {
